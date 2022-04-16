d00c0d6ab1871aca61791ff7fe08e62868dfa527
lvch0 <100215416+lvch0@users.noreply.github.com>
2022-04-15 23:44:15 -0300

    Ejercicio finalizado

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -1,10 +1,10 @@
 import random
 
 num = random.randint(1, 100)
-entrada = int(input("Ingrese un valor que se encuentre entre 1 y 100:"))
 intentos = 10
 
 while(intentos != 0):
+    entrada = int(input("Ingrese un valor que se encuentre entre 1 y 100:"))
     if(entrada == num):
         print("¡¡¡Felicitaciones, haz acertado!!!")
         break
@@ -16,8 +16,14 @@ while(intentos != 0):
         print("Por favor ingrese un valor del 1 al 100")
         break
     
-    intentos -= intentos
-    print(f"Le quedan {intentos} intentos.")
+    intentos = intentos - 1
+    
+    if(intentos == 0):
+        print("Ya no quedan intentos.")
+        break
+    else:
+        print(f"Le quedan {intentos} intentos.")
+    
     
     
     
